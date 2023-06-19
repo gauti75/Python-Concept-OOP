61753a2636f144d085545e7fbaec32e06326d25d
Gautam Gandotra <119655766+gauti75@users.noreply.github.com>
2023-06-19 14:06:05 -0400

    Add More Examples for simplification

--- Classes.py
+++ Classes.py
@@ -28,3 +28,32 @@ print(y.display())
 
 
 
+#### Another Basic Example of Python Classes  ####
+
+
+print("\n\n Second Example \n\n")
+class Mobile:
+    model_name=None
+    mfg_year=None
+    camera_pixel=None
+
+    def __init__(self,model_name,mfg_year,camera_pixel):
+        self.model_name=model_name
+        self.mfg_year=mfg_year
+        self.camera_pixel=camera_pixel
+
+    def display(self):
+        print("This is your phone details\n")
+        print("Model_name: "+self.model_name)
+        print(f"Manufacturing Year: {self.mfg_year}")
+        print(f"Pixel in this model: {self.camera_pixel}")
+
+    
+
+    
+mobile_1=Mobile("Galaxy FE",2020,"64 px")
+
+mobile_1.display()
+
+
+
